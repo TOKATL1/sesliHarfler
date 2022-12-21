3c46ab9d3854487d20865de5664ad13ba6538547
TOKATL1 <120321214+TOKATL1@users.noreply.github.com>
2022-12-21 18:18:52 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,90 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        char harf='B';
+
+      switch (harf){
+
+
+
+          case 'A':
+          case 'I':
+          case 'O':
+          case 'U':
+System.out.println("KALIN SESLİ HARFLER ");
+
+break;
+          default:System.out.println("ince sesli harfler")
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
